81b2c2460d397aa6aa0c3677a7fe48637cee1a2b
System Administrator <root@JOMACK.local>
2016-02-01 09:54:07 -0500

    My first app with typescript. Demo Lab for meetup at Microsoft

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'views/template1.html'
+})
+
+export class AppComponent {}
+
+bootstrap(AppComponent);
